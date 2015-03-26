2039bb51066facf92f88dc80b7d538de56eea20a
stefan0722 <stefan.henneberger@salzburgresearch.at>
2015-03-26 16:14:48 +0100

    add slippy map tiles controller

---
+++ src/main/java/org/geoserver/slippymap/SlippyMapTileCalculator.java
@@ -0,0 +1,140 @@
+package org.geoserver.slippymap;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.referencing.CRS;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.geotools.referencing.datum.DefaultEllipsoid;
+import org.geotools.util.logging.Logging;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Tile calculator that converts between slippy map tiles and coordiante bounds in EPSG 4326 format.
+ *
+ */
+public class SlippyMapTileCalculator {
+
+    private static CoordinateReferenceSystem CRS_3857;
+
+    private static CoordinateReferenceSystem CRS_4326;
+
+    private static final Logger LOGGER = Logging.getLogger(SlippyMapTileCalculator.class);
+
+    static {
+        try {
+            CRS_3857 = CRS.decode("EPSG:3857");
+            CRS_4326 = CRS.decode("EPSG:4326");
+        } catch (FactoryException e) {
+            LOGGER.log(Level.WARNING,"CRS could not be created",e);
+        }
+    }
+
+    /**
+     * Return the boundingbox in degree (EPSG:4326) for the requested Slippy Map Tile in the zoom level specified by the
+     * argument.
+     *
+     * @param x the Slippy Map xTile
+     * @param y the Slippy Map yTile
+     * @param zoom the Slippy Map zoom Level
+     * @param srid the reference system of the resulting bbox. Either 4326 (expensive), 900913 or 3857 (less expensive)
+     * @return a boundingbox containing the min and max lat / lon values in EPSG:4326
+     */
+    public static ReferencedEnvelope tile2boundingBox(final int x, final int y, final int zoom, int srid) {
+        ReferencedEnvelope referencedEnvelope = null;
+        switch (srid) {
+            case 4326:
+                referencedEnvelope = new ReferencedEnvelope(
+                        tile2lon(x, zoom),
+                        tile2lon(x + 1, zoom),
+                        tile2lat(y + 1, zoom),
+                        tile2lat(y, zoom), CRS_4326);
+                break;
+            case 3857:
+            case 900913:
+                referencedEnvelope = new ReferencedEnvelope(
+                        tile2xMercator(x,zoom),
+                        tile2xMercator(x + 1, zoom),
+                        tile2yMercator(y+1,zoom),
+                        tile2yMercator(y, zoom),
+                        CRS_3857);
+                break;
+            default:
+                throw new IllegalArgumentException(srid + " is not allowed please use 4326, 900913 or 3857");
+        }
+        return referencedEnvelope;
+    }
+
+    /**
+     * Returns the longitude for an x tile
+     *
+     * @param x the Slippy Map xTile
+     * @param z the Slippy Map zoom Level
+     * @return a longitude in decimal degree
+     */
+    public static double tile2lon(int x, int z) {
+        return x / Math.pow(2.0, z) * 360.0 - 180;
+    }
+
+    /**
+     * Returns the x value for the google mercator projection (EPSG:3857)
+     *
+     * @param x the Slippy Map xTile
+     * @param z the Slippy Map zoom Level
+     * @return a x coordinate in google mercator
+     */
+    public static double tile2xMercator(int x, int z) {
+        double n = Math.pow(2.0, z);
+        return DefaultEllipsoid.WGS84.getSemiMajorAxis() * Math.PI * (2 * x / n - 1);
+    }
+
+    /**
+     * Returns the longitude for an y tile
+     *
+     * @param y the Slippy Map yTile
+     * @param z the Slippy Map zoom Level
+     * @return a latitude in decimal degree
+     */
+    public static double tile2lat(int y, int z) {
+        double n = Math.PI - (2.0 * Math.PI * y) / Math.pow(2.0, z);
+        return Math.toDegrees(Math.atan(Math.sinh(n)));
+    }
+
+    /**
+     * Returns the y value in mercator projection (EPSG:3857)
+     *
+     * @param y the Slippy Map y tile
+     * @param z the Slippy Map zzom Level
+     * @return y coordinate in google mercator
+     */
+    public static double tile2yMercator(int y, int z) {
+        double n = Math.pow(2.0, z);
+        return DefaultEllipsoid.WGS84.getSemiMajorAxis() * Math.PI * (1 - (2*y/n));
+    }
+
+    /**
+     * Returns the Slippy Map xTile from a position given in lon / lat (EPSG:4326)
+     * @param lat the latitude
+     * @param lon the longitude
+     * @param zoom the zoomlevel which should be considered
+     * @return the xTile value
+     */
+    public static int getXTile(final double lat, final double lon, final int zoom) {
+        return (int) Math.floor((lon + 180) / 360 * (1 << zoom));
+    }
+
+    /**
+     * Returns the Slippy Map yTile from a position given in lon / lat (EPSG:4326)
+     * @param lat the latitude
+     * @param lon the longitude
+     * @param zoom the zoomlevel which should be considered
+     * @return the yTile value
+     */
+    public static int getYTile(final double lat, final double lon, final int zoom) {
+        return (int) Math.floor((1 - Math.log(Math.tan(Math.toRadians(lat)) + 1 / Math.cos(Math.toRadians(lat))) / Math.PI) / 2 * (1 << zoom));
+    }
+
+}
---
+++ src/main/java/org/geoserver/slippymap/SlippyTilesController.java
@@ -0,0 +1,117 @@
+package org.geoserver.slippymap;
+
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.opengis.referencing.ReferenceIdentifier;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Slippy Map Tiles controller that converts the requests into WMS requests. Answers with an redirect to the WMS service
+ * so that the security rules of the WMS service are used.
+ *
+ */
+@Controller
+public class SlippyTilesController {
+
+    private int defaultBuffer = 10;
+    private int defaultTileSize = 256;
+    private String defaultFormat = "application/x-protobuf";
+    private String defaultStyles = "";
+
+    private Map<String,String> supportedOutputFormats;
+
+    @RequestMapping(value="{layers}/{z}/{x}/{y}.{format}", method = RequestMethod.GET)
+    public void doGetSlippyWmsMap(
+            @PathVariable String layers,
+            @PathVariable int z,
+            @PathVariable int x,
+            @PathVariable int y,
+            @PathVariable String format,
+            @RequestParam(value = "buffer", required = false) Integer buffer,
+            @RequestParam(value = "tileSize", required = false) Integer tileSize,
+            @RequestParam(value = "styles", required = false) String styles,
+            @RequestParam(value = "time",required = false) String time,
+            @RequestParam(value = "sld", required = false) String sld,
+            @RequestParam(value = "sld_body", required = false) String sld_body,
+            final HttpServletResponse response) throws IOException {
+
+        //Build relative WMS redirect URL from Path Variables and optional request params
+        ReferencedEnvelope bbox = SlippyMapTileCalculator.tile2boundingBox(x, y, z,3857);
+        StringBuilder sb = new StringBuilder("/geoserver/wms?");
+        sb.append("STYLES=").append(styles != null ? styles : defaultStyles);
+        sb.append("&LAYERS=").append(layers);
+        sb.append("&FORMAT=").append(format != null && supportedOutputFormats != null
+                && !supportedOutputFormats.isEmpty() ? supportedOutputFormats.get(format) : defaultFormat);
+        sb.append("&SERVICE=").append("WMS");
+        sb.append("&VERSION=").append("1.1.1");
+        sb.append("&REQUEST=").append("GetMap");
+        sb.append("&SRS=").append(getCRSIdentifier(bbox.getCoordinateReferenceSystem()));
+        sb.append("&BBOX=").append(bbox.getMinX()).append(',').append(bbox.getMinY())
+                .append(',').append(bbox.getMaxX()).append(',').append(bbox.getMaxY());
+        sb.append("&WIDTH=").append(tileSize != null ? tileSize : defaultTileSize);
+        sb.append("&HEIGHT=").append(tileSize != null ? tileSize : defaultTileSize);
+        sb.append("&BUFFER=").append(buffer != null ? buffer : defaultBuffer);
+        if (time != null) {
+            sb.append("&TIME=").append(time);
+        }
+        if (sld != null) {
+            sb.append("&SLD=").append(sld);
+        }
+        if (sld_body != null) {
+            sb.append("&SLD_BODY=").append(sld_body);
+        }
+        String url = sb.toString();
+        response.sendRedirect(response.encodeRedirectURL(url));
+    }
+
+    private String getCRSIdentifier(CoordinateReferenceSystem coordinateReferenceSystem) {
+        String name = "";
+        if (coordinateReferenceSystem.getIdentifiers() == null) {
+            name = coordinateReferenceSystem.getName().toString();
+        } else {
+            for (ReferenceIdentifier identifier : coordinateReferenceSystem.getIdentifiers()) {
+                name = identifier.toString();
+                break;
+            }
+        }
+        return name;
+    }
+
+    public void setDefaultBuffer(int defaultBuffer) {
+        this.defaultBuffer = defaultBuffer;
+    }
+
+    public void setDefaultTileSize(int defaultTileSize) {
+        this.defaultTileSize = defaultTileSize;
+    }
+
+    public void setDefaultFormat(String defaultFormat) {
+        this.defaultFormat = defaultFormat;
+    }
+
+    public void setDefaultStyles(String defaultStyles) {
+        this.defaultStyles = defaultStyles;
+    }
+
+    public Map<String, String> getSupportedOutputFormats() {
+        return supportedOutputFormats;
+    }
+
+    /**
+     * Mapping of file endings to mime types / output formats know by the geoserver. Unfortunately file endings are not
+     * recored by the {@link org.geoserver.wms.GetMapOutputFormat} interface so this has to be entered here manually.
+     *
+     * @param supportedOutputFormats the outputFormat to file ending mapping
+     */
+    public void setSupportedOutputFormats(Map<String, String> supportedOutputFormats) {
+        this.supportedOutputFormats = supportedOutputFormats;
+    }
+}
